ee44192228bbab0853cda47ace7e0c9ddf715917
ZHUYUZHEN <39178670+ZHUYUZHEN@users.noreply.github.com>
2018-05-27 15:45:03 +0800

    這是使用指向非常數資料的非常數指標的撰寫方式

---
+++ bubbleSort.c
@@ -0,0 +1,21 @@
+void bubbleSort(int *array,int size)
+{
+	void swap(int *element1,int *element2);
+	unsigned int pass;
+	int i;
+	for(pass=1;pass<10;++pass){
+		for(i=0;i<10;++i){
+			if(array[i]>array[i+1]){
+				swap(&array[i],&array[i+1]);
+				
+			}
+		}
+	}
+
+}
+
+void swap(int *element1,int *element2){
+	int hold=*element1;
+	*element1=*element2;
+	*element2=hold;
+}
---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void bubbleSort(int *array, size_t size);
+
+int main(int argc, char *argv[]) {
+	int x[SIZE]={5,8,12,26,56,4,45,9,2,8};
+	size_t i;
+	puts("Original Data:");
+		for (i=0;i<SIZE;++i)
+	{
+		printf("%4d",x[i]);
+	}
+	
+	bubbleSort(x,SIZE);
+	
+	puts("\nAscending Data:");
+	for(i=0;i<SIZE;++i){
+		printf("%4d",x[i]);
+	}
+}
